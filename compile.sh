2bd15a143daf6d0e834fc21ab109b30896ad021a
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2021-12-02 17:46:44 +0000

    Compile script

---
+++ compile.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Check if sdl2 is installed
+if [[ $(command -v sdl2-config) == "" ]]; then
+	echo "Cant' find sdl2. Exiting..."
+	exit 1;
+fi
+
+SDL_FLAGS=$(sdl2-config --cflags --libs)
+OUTPUT_FILE="tv-noise.o"
+
+if [[ $1 == "" ]]; then
+	echo "No arguments. Please enter a file. Exiting..."
+	exit 1;
+fi
+
+# Compile the file
+gcc -o $OUTPUT_FILE $1 $SDL_FLAGS
+
+if [[ $2 == "comp" ]]; then
+	echo "Compiled! Running..."
+	./$OUTPUT_FILE
+fi
